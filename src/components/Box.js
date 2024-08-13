3f7ec9d7d6a8170d74d85133c8b4c3be4ea821c7
Hyejung Na <hyejung.na01@gmail.com>
2024-08-14 01:11:35 +1000

    add default image

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 const Box = (props) => {
-  console.log(props);
+  // console.log(props);
+  const defaultImage =
+    'https://hips.hearstapps.com/hmg-prod/images/people-playing-paper-rock-scissors-royalty-free-illustration-1583269312.jpg';
+
   return (
     <div className='box'>
       <h1>{props.title}</h1>
       <img
         className='item-img'
-        src={props.item && props.item.img}
+        src={props.item && props.item.img ? props.item.img : defaultImage}
         alt='user-choice'
       ></img>
       <h2>Win</h2>
