40a630fa0f1f413e526b5d40526ca633b60d1ad0
Hyejung Na <hyejung.na01@gmail.com>
2024-08-15 01:43:00 +1000

    update game logics and  styling

--- src/App.js
+++ src/App.js
@@ -1,36 +1,88 @@
 import { useState } from 'react';
-import './App.css';
+import './styles/App.css';
 import Box from './components/Box';
+import rockImg from './assets/rock-img.webp';
+import paperImg from './assets/paper-img.jpg';
+import scissorsImg from './assets/scissors-img.jpeg';
 
 const choice = {
   rock: {
     name: 'Rock',
-    img: 'https://cdn.pixabay.com/photo/2014/03/25/15/26/rock-paper-scissors-296854_1280.png',
+    img: rockImg,
+    // 'https://cdn.pixabay.com/photo/2014/03/25/15/26/rock-paper-scissors-296854_1280.png'
   },
   paper: {
     name: 'Paper',
-    img: 'https://www.teachersuperstore.com.au/assets/alt_5/PA313.jpg?20210904233250',
+    img: paperImg,
+    // 'https://www.teachersuperstore.com.au/assets/alt_5/PA313.jpg?20210904233250',
   },
   scissors: {
     name: 'Scissors',
-    img: 'https://thumb.ac-illust.com/19/19c97f48d78aa6eaf21e77af1d4e1bd3_t.jpeg',
+    img: scissorsImg,
+    // 'https://thumb.ac-illust.com/19/19c97f48d78aa6eaf21e77af1d4e1bd3_t.jpeg',
   },
 };
 
 function App() {
   const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState('');
 
   const play = (userChoice) => {
     setUserSelect(choice[userChoice]);
+
+    const computerChoice = randomChoice();
+    setComputerSelect(computerChoice);
+
+    setResult(judgement(choice[userChoice], computerChoice));
+  };
+
+  const judgement = (user, computer) => {
+    console.log(user, computer);
+
+    if (user.name === computer.name) {
+      return 'tie';
+    } else if (user.name === 'Rock')
+      return computer.name === 'Scissors' ? 'win' : 'lose';
+    else if (user.name === 'Scissors')
+      return computer.name === 'Paper' ? 'win' : 'lose';
+    else if (user.name === 'Paper')
+      return computer.name === 'Rock' ? 'win' : 'lose';
+  };
+
+  const randomChoice = () => {
+    const itemArray = Object.keys(choice);
+    // console.log(itemArray);
+
+    const randomItem = Math.floor(Math.random() * itemArray.length);
+    // console.log(randomItem);
+
+    const final = itemArray[randomItem];
+    return choice[final];
+  };
+
+  const invertResult = (result) => {
+    switch (result) {
+      case 'win':
+        return 'lose';
+      case 'lose':
+        return 'win';
+      default:
+        return result; // 'tie' remains 'tie'
+    }
   };
 
   return (
     <div>
-      <div className='main'>
-        <Box title='You' item={userSelect} />
-        {/* <Box title='Computer' /> */}
+      <div className='box-main'>
+        <Box title='You' item={userSelect} result={result} />
+        <Box
+          title='Computer'
+          item={computerSelect}
+          result={invertResult(result)}
+        />
       </div>
-      <div className='main'>
+      <div className='button-main'>
         <button className='button' onClick={() => play('rock')}>
           rock
         </button>
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import rockScissorsPaperImg from '../assets/people-playing-paper-rock-scissors.jpg';
 
 const Box = (props) => {
   // console.log(props);
-  const defaultImage =
-    'https://hips.hearstapps.com/hmg-prod/images/people-playing-paper-rock-scissors-royalty-free-illustration-1583269312.jpg';
+
+  const defaultImage = rockScissorsPaperImg;
+  // 'https://hips.hearstapps.com/hmg-prod/images/people-playing-paper-rock-scissors-royalty-free-illustration-1583269312.jpg'
+
+  const borderColor =
+    props.result === 'win'
+      ? 'box-win'
+      : props.result === 'lose'
+      ? 'box-lose'
+      : 'box-tie';
 
   return (
-    <div className='box'>
-      <h1>{props.title}</h1>
+    <div className={`box ${borderColor}`}>
+      <h1 className='box-title'>{props.title}</h1>
       <img
         className='item-img'
         src={props.item && props.item.img ? props.item.img : defaultImage}
         alt='user-choice'
       ></img>
-      <h2>Win</h2>
+      <h2 className='box-result'>{props.result}</h2>
     </div>
   );
 };
